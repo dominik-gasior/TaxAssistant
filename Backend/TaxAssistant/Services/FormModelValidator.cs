dfc1d29990c2ee997df1b4122511984378b55b41
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2024-09-29 09:59:09 +0200

    update validator

--- Backend/TaxAssistant/Services/FormModelValidator.cs
+++ Backend/TaxAssistant/Services/FormModelValidator.cs
@@ -9,13 +9,73 @@ public class FormModelValidator
     {
         var valid = ValidateForm(newModel);
 
+        var address = UpdateAddress(oldModel.Address, newModel.Address, valid);
+        IndividualTaxpayer individualTaxpayer = new();
+        CompanyTaxpayer companyTaxpayer = new();
+        if (newModel.TaxpayerType == "individual")
+        {
+            var newIndividualTaxpayer = JsonSerializer.Deserialize<IndividualTaxpayer>(JsonSerializer.Serialize(newModel.TaxpayerData)) ?? new IndividualTaxpayer();
+            var oldIndividualTaxpayer = oldModel.TaxpayerType == "individual"
+                ? JsonSerializer.Deserialize<IndividualTaxpayer>(JsonSerializer.Serialize(newModel.TaxpayerData)) ?? new IndividualTaxpayer()
+                : new IndividualTaxpayer();
+            individualTaxpayer = UpdateIndividualTaxpayer(oldIndividualTaxpayer, newIndividualTaxpayer, valid);
+        }
+        if (newModel.TaxpayerType == "company")
+        {
+            var newIndividualTaxpayer = JsonSerializer.Deserialize<CompanyTaxpayer>(JsonSerializer.Serialize(newModel.TaxpayerData)) ?? new CompanyTaxpayer();
+            var oldIndividualTaxpayer = oldModel.TaxpayerType == "company"
+                ? JsonSerializer.Deserialize<CompanyTaxpayer>(JsonSerializer.Serialize(newModel.TaxpayerData)) ?? new CompanyTaxpayer()
+                : new CompanyTaxpayer();
+            companyTaxpayer = UpdateCompanyTaxpayer(oldIndividualTaxpayer, newIndividualTaxpayer, valid);
+        }
+        
+        var result = oldModel with
+        {
+            ActionDescription = valid.Contains(nameof(oldModel.ActionDescription)) ? newModel.ActionDescription : oldModel.ActionDescription,
+            Amount = valid.Contains(nameof(oldModel.Amount)) ? newModel.Amount : oldModel.Amount,
+            DateOfAction = valid.Contains(nameof(oldModel.DateOfAction)) ? newModel.DateOfAction : oldModel.DateOfAction,
+            EntitySubmittingAction = valid.Contains(nameof(oldModel.EntitySubmittingAction)) ? newModel.EntitySubmittingAction : oldModel.EntitySubmittingAction,
+            OfficeName = valid.Contains(nameof(oldModel.OfficeName)) ? newModel.OfficeName : oldModel.OfficeName,
+            TaxpayerType = valid.Contains(nameof(oldModel.TaxpayerType)) ? newModel.TaxpayerType : oldModel.TaxpayerType,
+            Address = address,
+            TaxpayerData = newModel.TaxpayerType == "individual" ? individualTaxpayer : companyTaxpayer
+            
+        };
 
-
-        return oldModel;
+        return result;
     }
     
+    private IndividualTaxpayer UpdateIndividualTaxpayer(IndividualTaxpayer oldIndividual, IndividualTaxpayer newIndividual, List<string> valid) => new()
+    {
+        FirstName = valid.Contains(nameof(oldIndividual.FirstName)) ? newIndividual.FirstName : oldIndividual.FirstName,
+        LastName = valid.Contains(nameof(oldIndividual.LastName)) ? newIndividual.LastName : oldIndividual.LastName,
+        Pesel = valid.Contains(nameof(oldIndividual.Pesel)) ? newIndividual.Pesel : oldIndividual.Pesel,
+        DateOfBirth = valid.Contains(nameof(oldIndividual.DateOfBirth)) ? newIndividual.DateOfBirth : oldIndividual.DateOfBirth,
+    };
+    
+    private CompanyTaxpayer UpdateCompanyTaxpayer(CompanyTaxpayer oldCompany, CompanyTaxpayer newCompany, List<string> valid) => new()
+    {
+        FullName = valid.Contains(nameof(oldCompany.FullName)) ? newCompany.FullName : oldCompany.FullName,
+        ShortName = valid.Contains(nameof(oldCompany.ShortName)) ? newCompany.ShortName : oldCompany.ShortName,
+        NIP = valid.Contains(nameof(oldCompany.NIP)) ? newCompany.NIP : oldCompany.NIP,
+    };
+
+    private Address UpdateAddress(Address oldAddress, Address newAddress, List<string> valid) => new()
+    {
+        Country = valid.Contains(nameof(oldAddress.Country)) ? newAddress.Country : oldAddress.Country,
+        Province = valid.Contains(nameof(oldAddress.Province)) ? newAddress.Province : oldAddress.Province,
+        PostalCode = valid.Contains(nameof(oldAddress.PostalCode)) ? newAddress.PostalCode : oldAddress.PostalCode,
+        Street = valid.Contains(nameof(oldAddress.Street)) ? newAddress.Street : oldAddress.Street,
+        ApartmentNumber = valid.Contains(nameof(oldAddress.ApartmentNumber)) ? newAddress.ApartmentNumber : oldAddress.ApartmentNumber,
+        Municipality = valid.Contains(nameof(oldAddress.Municipality)) ? newAddress.Municipality : oldAddress.Municipality,
+        County = valid.Contains(nameof(oldAddress.County)) ? newAddress.County : oldAddress.County,
+        HouseNumber = valid.Contains(nameof(oldAddress.HouseNumber)) ? newAddress.HouseNumber : oldAddress.HouseNumber,
+        City = valid.Contains(nameof(oldAddress.City)) ? newAddress.City : oldAddress.City
+    };
+
     public List<string> ValidateForm(FormModel formModel)
     {
+        //TODO: officeName
         var validProps = new List<string>();
         if (formModel.DateOfAction > DateOnly.FromDateTime(DateTime.Now.AddDays(-14))) validProps.Add(nameof(formModel.DateOfAction));
         if (formModel.OfficeName is "") validProps.Add(nameof(formModel.OfficeName));
@@ -83,4 +143,3 @@ public class FormModelValidator
         return validProps;
     }
 }
-
