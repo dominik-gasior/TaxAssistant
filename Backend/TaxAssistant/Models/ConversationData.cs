d939af732791c5b67d174cb3ab028519a77ca397
KiobaDev <jakub.sarecki.developer@gmail.com>
2024-09-29 09:51:17 +0200

    Models

--- Backend/TaxAssistant/Models/ConversationData.cs
+++ Backend/TaxAssistant/Models/ConversationData.cs
@@ -1,14 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace TaxAssistant.Models;
 
 public record ConversationData
 {
     public required string Id { get; init; }
+    
+    [JsonPropertyName("messages")]
     public required List<Message> ChatLog { get; init; } = [];
+    
+    [JsonPropertyName("formData")]
     public required FormModel FormModel { get; init; }
 }
 
 public record Message
 {
+    [JsonPropertyName("timestamp")]
     public required long TimeStamp { get; init; }
     public required string Role { get; init; }
     public required string Content { get; init; }
